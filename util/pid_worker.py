1e598f91172a67ef44281e8559907ea1663d6d4d
greenmapc <greenmapc@gmail.com>
2020-05-05 20:32:29 +0300

    refuction host queries for getting cluster info

--- util/pid_worker.py
+++ util/pid_worker.py
@@ -1,13 +1,17 @@
 import domain.query_constant
-from util.workload_service import calculate_process_workload, get_metrics
+from util.workload_service import calculate_process_workload, get_metrics, get_info_about_all_pg_processes, \
+    metrics_to_dict
 
 
 def select_resource_intensive_process(processes, host_connection, wm_db_connection):
     metrics = get_metrics(wm_db_connection)
     # todo ?maybe need kill recent process
+    processes_info = get_info_about_all_pg_processes(host_connection, processes,
+                                                     list(map(lambda metric: metric['name'], metrics)))
+    metrics = metrics_to_dict(metrics)
     result = max(zip(
-        map(lambda process: calculate_process_workload(process, metrics, host_connection), processes),
-        map(lambda process: process.pid, processes))
+        map(lambda process: calculate_process_workload(process, metrics), processes_info),
+        map(lambda process: process['pid'], processes_info))
     )
     return result[1]
 
