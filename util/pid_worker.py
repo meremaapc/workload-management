1ad2db5e17fc53e97cdde1672e9c1a13f4921978
greenmapc <greenmapc@gmail.com>
2020-05-23 17:10:28 +0300

    added metric entity, workload formula

--- util/pid_worker.py
+++ util/pid_worker.py
@@ -1,16 +1,18 @@
 import domain.query_constant
-from config import CLIENT_BACKEND, HINT
+from config import CLIENT_BACKEND, HINT, CPU_PARAM, RAM_PARAM, PID_PARAM
 from util.workload_service import calculate_process_workload, get_metrics, get_info_about_all_pg_processes
 
 
 def select_resource_intensive_process(processes, host_connection, wm_db_connection):
-    processes = list(filter(lambda item: str(item.backend_type) == CLIENT_BACKEND and HINT not in str(item.query), processes))
+    processes = list(
+        filter(lambda item: str(item.backend_type) == CLIENT_BACKEND and HINT not in str(item.query), processes))
     metrics = get_metrics(wm_db_connection)
-    processes_info = get_info_about_all_pg_processes(host_connection, processes, list(map(lambda metric: metric['name'], metrics)))
+    processes_info = get_info_about_all_pg_processes(host_connection, processes,
+                                                     list(map(lambda metric: metric.name, metrics)))
     result = max(zip(
-        map(lambda process: calculate_process_workload(float(process['pcpu']), float(process['pmem']), metrics),
-            processes_info),
-        map(lambda process: process['pid'], processes_info))
+        map(lambda process: calculate_process_workload(
+                {CPU_PARAM: float(process[CPU_PARAM]), RAM_PARAM: float(process[RAM_PARAM])}, metrics), processes_info),
+        map(lambda process: process[PID_PARAM], processes_info))
     )
     return result[1]
 
